369613d02b49eb7cbf5a4e96580332bc6d2f8984
Navmonti <navid.montazeripour@gmail.com>
2022-08-23 23:53:59 +0430

    add project Controller add project Service add project Interface Add project DTOs Add Project Errors to AppResource

---
+++ src/Avn.Api/Controllers/ProjectController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avn.Api.Controllers
+{
+    public class ProjectController : BaseController
+    {
+        private readonly IProjectService _projectService;
+        public ProjectController(IProjectService projectService) => _projectService = projectService;
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(Guid userId, [FromBody] Project item)
+        {
+            item.UserId = userId;
+            return Ok(await _projectService.CreateAsync(item));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAsync(Guid userId, [FromBody] Project item)
+        {
+            item.UserId = userId;
+            return Ok(await _projectService.UpdateAsync(item));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(Guid userId)
+            => Ok(await _projectService.GetAllAsync(userId));
+    }
+}
---
+++ src/Avn.Domain/Dtos/Projects/ProjectDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avn.Domain.Dtos.Projects
+{
+    public class ProjectDto
+    { 
+        public Guid Id { get; set; }
+        public Guid? UserId { get; set; } 
+        public string Name { get; set; }
+        public string SourceIp { get; set; }
+        public string Website { get; set; }
+        public string ApiKey { get; set; } 
+    }
+}
