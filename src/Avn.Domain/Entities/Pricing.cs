3a65eb31de356a0db84955bfd8379071a3da6027
mohammadKarimi <mha.karimi@gmail.com>
2022-09-01 22:14:56 +0430

    Add Pricing Model

---
+++ src/Avn.Domain/Entities/Pricing.cs
@@ -0,0 +1,25 @@
+﻿namespace Avn.Domain.Entities;
+
+[Table(nameof(Pricing), Schema = nameof(EntitySchema.Base))]
+public class Pricing
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string AvailableNetworks { get; set; }
+
+    [Required]
+    public int Price { get; set; }
+
+    [Required]
+    public int RequestsPerSecond { get; set; }
+
+    public bool IsActive { get; set; }
+}
--- src/Avn.Domain/Enums/EmailType.cs
+++ src/Avn.Domain/Enums/EmailType.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Avn.Domain.Enums;
 
-namespace Avn.Domain.Enums
+public enum EmailTemplate : byte
 {
-    public enum EmailTemplate : byte
-    {
-        Verification = 1,
-        Registration = 2,
-        ForgetPassword = 3
-    }
+    Verification = 1,
+    Registration = 2,
+    ForgetPassword = 3
 }
