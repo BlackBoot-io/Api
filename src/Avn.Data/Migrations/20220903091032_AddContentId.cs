19722582a4751e51ad20198351041f1c9bfb3bda
mohammadKarimi <mha.karimi@gmail.com>
2022-09-03 13:41:28 +0430

    Add ContentId in Drop entity

---
+++ src/Avn.Data/Migrations/20220903091032_AddContentId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Avn.Data.Migrations
+{
+    public partial class AddContentId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentId",
+                schema: "User",
+                table: "Drops",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentId",
+                schema: "User",
+                table: "Drops");
+        }
+    }
+}
