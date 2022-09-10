710a53542fa7586fdcee6315f33003277a5a8dc3
mohammadKarimi <mha.karimi@gmail.com>
2022-09-10 17:39:05 +0430

    summary on project's actions

--- src/Avn.Api/Controllers/ProjectsController.cs
+++ src/Avn.Api/Controllers/ProjectsController.cs
@@ -5,6 +5,12 @@ public class ProjectsController : BaseController
     private readonly IProjectsService _projectService;
     public ProjectsController(IProjectsService projectService) => _projectService = projectService;
 
+    /// <summary>
+    /// Create a Project and get api-key to integration
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="item"></param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<IActionResult> AddAsync(Guid userId, [FromBody] CreateProjectDto item)
     {
@@ -12,6 +18,12 @@ public class ProjectsController : BaseController
         return Ok(await _projectService.CreateAsync(item));
     }
 
+    /// <summary>
+    /// update name,ip,website of a project
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="item"></param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<IActionResult> UpdateAsync(Guid userId, [FromBody] UpdateProjectDto item)
     {
@@ -19,6 +31,11 @@ public class ProjectsController : BaseController
         return Ok(await _projectService.UpdateAsync(item));
     }
 
+    /// <summary>
+    /// Get all user's project with userId
+    /// </summary>
+    /// <param name="userId">Primary key of a user</param>
+    /// <returns></returns>
     [HttpGet]
     public async Task<IActionResult> GetAllAsync(Guid userId)
         => Ok(await _projectService.GetAllAsync(userId));
