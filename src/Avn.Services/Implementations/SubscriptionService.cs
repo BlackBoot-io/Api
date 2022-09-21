1c61a7464b628a5ea4536d07ae9c1daa12e3a7db
mohammadKarimi <mha.karimi@gmail.com>
2022-09-21 23:09:16 +0330

    Add Comments

--- src/Avn.Services/Implementations/SubscriptionService.cs
+++ src/Avn.Services/Implementations/SubscriptionService.cs
@@ -2,6 +2,11 @@
 
 public class SubscriptionService : ISubscriptionService
 {
+    /// <summary>
+    /// Get current subscription model for a user
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
     public Task<IActionResponse<Subscription>> GetCurrentModelAsync(Guid userId)
     {
       
