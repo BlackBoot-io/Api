53fc07029328c2f5575d9be98ef0c453cff21984
mohammadKarimi <mha.karimi@gmail.com>
2022-09-21 23:08:38 +0330

    Remove implementation of Qr

--- src/Avn.Services/Implementations/DeliveryStrategies/QrStrategy.cs
+++ src/Avn.Services/Implementations/DeliveryStrategies/QrStrategy.cs
@@ -5,5 +5,5 @@ namespace Avn.Services.Implementations.DeliveryStrategies;
 public class QrStrategy : IDeliveryStrategy
 {
     public async Task<IActionResponse<byte[]>> ExecuteAsync(int dropId, int count, CancellationToken cancellationToken)
-      => new ActionResponse<byte[]>();
+       
 }
