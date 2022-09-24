55e0eb8bdb66c6235afad83d04a68a531a10358f
mohammadKarimi <mha.karimi@gmail.com>
2022-09-24 17:26:44 +0330

    Add Comments

--- src/Avn.Api/Controllers/VerificationController.cs
+++ src/Avn.Api/Controllers/VerificationController.cs
@@ -5,9 +5,14 @@ namespace Avn.Api.Controllers;
 public class VerificationController : BaseController
 {
     private readonly IVerificationService _verificationService;
+
     public VerificationController(IVerificationService verificationService)
         => _verificationService = verificationService;
 
+    /// <summary>
+    /// Resend Verification Code
+    /// </summary>
+    /// <returns></returns>
     [HttpPost]
     public async Task<IActionResult> ResendCode()
      => Ok(await _verificationService.SendOtpAsync(CurrentUserId, TemplateType.ResendCode));
