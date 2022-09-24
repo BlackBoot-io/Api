8b88693fedf115a8340c5db916c79dca39b5711c
mohammadKarimi <mha.karimi@gmail.com>
2022-09-24 15:15:50 +0330

    Implement Service

---
+++ src/Avn.Services/Implementations/NetworkService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avn.Services.Implementations;
+
+public class NetworkService : INetworkService
+{
+    public Task<IActionResponse<List<Network>>> GetAllAvailableAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
+
