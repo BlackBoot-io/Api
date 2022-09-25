bd9ae2c79695dac561e537ea49a25f626c5a2eaf
mohammadKarimi <mha.karimi@gmail.com>
2022-09-25 10:23:49 +0330

    Add ctor and inject IdropService

--- src/Avn.Api/Controllers/DropController.cs
+++ src/Avn.Api/Controllers/DropController.cs
@@ -2,5 +2,11 @@
 
 public class DropController : BaseController
 {
+    private readonly IDropsService _dropsService;
+    public DropController(IDropsService dropsService)
+    {
+        _dropsService = dropsService;
+    }
+
 
 }
