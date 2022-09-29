56762e4eebafc3a625f5f9f1efa70330e47275cc
mohammadKarimi <mha.karimi@gmail.com>
2022-09-29 23:42:10 +0330

    Chnage RandomString Generator with RNG cryptography

--- src/Avn.Shared/Utilities/RandomStringGenerator.cs
+++ src/Avn.Shared/Utilities/RandomStringGenerator.cs
@@ -1,13 +1,26 @@
-﻿namespace Avn.Shared.Utilities;
+﻿using System.Security.Cryptography;
+
+namespace Avn.Shared.Utilities;
 
 public static class RandomStringGenerator
 {
     private static readonly Random random = new();
 
-    public static string Generate(int length)
+#pragma warning disable CA1041 // Please Use Generate method to be secure
+    [Obsolete]
+#pragma warning restore CA1041 // Please Use Generate method to be secure
+    public static string GenerateUnsecure(int length)
     {
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    public static string Generate(int length)
+    {
+        var rng = RandomNumberGenerator.Create();
+        byte[] rno = new byte[length];
+        rng.GetBytes(rno);
+        return Convert.ToBase64String(rno);
     }
 }
