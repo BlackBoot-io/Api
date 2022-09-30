c394eff2b820e8c343704923ff637edc42969238
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2022-09-30 12:48:37 +0330

    migration

---
+++ src/Avn.Data/Migrations/20220930085049_UserLockout.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Avn.Data.Migrations
+{
+    public partial class UserLockout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLockoutEnabled",
+                schema: "Auth",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockoutEndDateUtc",
+                schema: "Auth",
+                table: "Users",
+                type: "datetime2",  
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLockoutEnabled",
+                schema: "Auth",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEndDateUtc",
+                schema: "Auth",
+                table: "Users");
+        }
+    }
+}
