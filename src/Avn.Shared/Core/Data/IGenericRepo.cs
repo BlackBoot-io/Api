c388a4b4e11747afce31f83bf2995f962832d7ba
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2022-10-06 12:27:27 +0330

    refactor

--- src/Avn.Shared/Core/Data/IGenericRepo.cs
+++ src/Avn.Shared/Core/Data/IGenericRepo.cs
@@ -3,10 +3,10 @@
 public interface IGenericRepo<TEntity> where TEntity : class
 {
     public IQueryable<TEntity> Queryable();
-    public Task<TEntity> FindAsync(object key, CancellationToken token = default);
-    public Task<TEntity> FindAsync(object[] keyValues, CancellationToken token = default);
-    public Task AddAsync(TEntity entity, CancellationToken token = default);
-    public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken token = default);
+    public Task<TEntity> FindAsync(object key, CancellationToken cancellationToken = default);
+    public Task<TEntity> FindAsync(object[] keyValues, CancellationToken cancellationToken = default);
+    public Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
     public void Update(TEntity entity);
     public void Remove(TEntity entity);
     public void RemoveRange(IEnumerable<TEntity> entities);
---
+++ src/Avn.Shared/Core/Services/Lazier.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Avn.Shared.Core.Services;
+public class Lazier<T> : Lazy<T> where T : class
+{
+    public Lazier(IServiceProvider provider)
+        : base(() => provider.GetRequiredService<T>())
+    {
+    }
+}
